3527f721a549cc964de6b6ca2506da520b087b6c
Fandi <fandi@zero-one-group.com>
2024-11-06 13:16:55 +0700

    feat: adjust mouse header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,12 @@
 export const Header = () => {
   return (
     <header
-      className="py-11 fixed top-0 inset-x-0 z-50"
+      className="py-11 fixed top-0 inset-x-0 z-50 pointer-events-none"
       style={{ mixBlendMode: "difference" }}
     >
       <div className="px-11 h-[120px] flex justify-between items-center">
-        <a className="font-sinisuka text-[48px]">Ilimitado Studio</a>
-        <button className="size-9 overflow-hidden">
+        <a className="font-sinisuka text-[48px] pointer-events-auto">Ilimitado Studio</a>
+        <button className="size-9 overflow-hidden pointer-events-auto">
           <div className="h-full w-full relative">
             <div className="w-full h-[2px] bg-foreground translate-y-[-6px] absolute inset-0 my-auto" />
             <div className="w-full h-[2px] bg-foreground translate-y-[6px] absolute inset-0 my-auto" />
